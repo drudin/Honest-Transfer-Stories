af7738922a9e6c040af0eaf281e365afe615406b
David Rudin <drudin@users.noreply.github.com>
2016-08-28 11:10:31 -0400

    Update content.js

--- content.js
+++ content.js
@@ -11,11 +11,11 @@ function troncIt() {
             if (node.nodeType === 3) {
                 var text = node.nodeValue;
                 var replacedText = text
-                                    .replace(/undisclosed fee/, 'a fee that is presumably rising to eighty bajillion dollars because nobody is willing to reveal the number');
-                                    .replace(/braced for a bid/, 'ready for another team to get in touch because that is how the sport works');
-                                    .replace(/option to buy/ig, 'option to pay the actual transfer fee later because the club does not have the money right now');
-                                    .replace(/slams shut/, 'gently closes just like everyone knew it would');
-                                    .replace(/Ars(e|è)ne(\W)?Wenger/, 'the successful manager who Arsenal fans periodically freak out about');
+                                    .replace(/undisclosed fee/, 'a fee that is presumably rising to eighty bajillion dollars because nobody is willing to reveal the number')
+                                    .replace(/braced for a bid/, 'ready for another team to get in touch because that is how the sport works')
+                                    .replace(/option to buy/ig, 'option to pay the actual transfer fee later because the club does not have the money right now')
+                                    .replace(/slams shut/, 'gently closes just like everyone knew it would')
+                                    .replace(/Ars(e|è)ne(\W)?Wenger/, 'the successful manager who Arsenal fans periodically freak out about')
                                     .replace(/pokemongo/ig, 'goingoutside')
                                     .replace(/(playing\W|played\W|play\W)?Pok(é|e)mon\WGo/gi, 'going outside')
                                     .replace(/(a\W)?Pok(é|e)mon/gi, 'fresh air')
