6effeed2ed10b45a28bcbf725fb19648c665eb2b
David Rudin <drudin@users.noreply.github.com>
2016-08-28 11:46:37 -0400

    Update content.js

--- content.js
+++ content.js
@@ -16,11 +16,17 @@ function troncIt() {
                                     .replace(/option to buy/ig, 'option to pay the actual transfer fee later because the club does not have the money right now')
                                     .replace(/slams shut/, 'gently closes just like everyone knew it would')
                                     .replace(/Ars(e|è)ne(\W)?Wenger/, 'the successful manager who Arsenal fans periodically freak out about')
-                                    .replace(/pokemongo/ig, 'goingoutside')
-                                    .replace(/(playing\W|played\W|play\W)?Pok(é|e)mon\WGo/gi, 'going outside')
-                                    .replace(/(a\W)?Pok(é|e)mon/gi, 'fresh air')
-                                    .replace(/lure\Wmodule/gi, 'big tent')
-                                    .replace(/Pok(è|é|e)(\W)?stop/ig, 'place outside');
+                                    .replace(/transfer request/, 'request to work somewhere else')
+                                    .replace(/wantaway/, 'keen on living or working somewhere else at this point in his life')
+                                    .replace(/star/, 'player of some value')
+                                    .replace(/trimming squad/, 'cutting down the size of their squad after spending unwisely in the past')
+                                    .replace(/under contract/, 'in a strictly legal sense obliged to ply his trade')
+                                    .replace(/agreed terms/, 'agreed to a significant pay package')
+                                    .replace(/not for sale/, 'not for sale, unless a bigger offer comes in, at which point he will definitely be for sale')
+                                    .replace(/target/, 'object of affection')
+                                    .replace(/important player/, 'man with two legs who runs around a bit')
+                                    .replace(/capture/, 'contractual agreement')
+                                    .replace(/negotiations/ 'talks between various parties and legal or financial experts');
 
                 if (replacedText !== text) {
                     node.textContent = replacedText;
