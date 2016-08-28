aa7922fdfb3beb8840bb98a495ea5f215d38feb8
David Rudin <drudin@users.noreply.github.com>
2016-08-28 14:14:16 -0400

    Update background.js

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ var toggle = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
   toggle = !toggle;
   if(toggle){
-    chrome.browserAction.setIcon({path: "icon128.png", tabId:tab.id});
+    chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
     chrome.tabs.executeScript(tab.id, {file:"content.js"});
   }
   else{
