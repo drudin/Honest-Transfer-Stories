196a25a34b6d7c134796cfb7956f53b7276df133
David Rudin <drudin@users.noreply.github.com>
2016-08-28 14:24:38 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,14 @@
-var toggle = false;
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  toggle = !toggle;
-  if(toggle){
-    chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
-    chrome.tabs.executeScript(tab.id, {file:"content.js"});
-  }
-  else{
-    chrome.browserAction.setIcon({path: "off.png", tabId:tab.id});
-    chrome.tabs.executeScript(tab.id, {code:"alert()"});
-  }
+var enable=false;
+chrome.browserAction.onClicked.addListener(function (tab) {
+ enable = enable ? false : true;
+ if(enable){
+  //turn on...
+  chrome.browserAction.setIcon({ path: 'icon128.png' });
+  chrome.browserAction.setBadgeText({ text: 'ON' });
+  chrome.tabs.executeScript(null, { file: 'content.js' }); 
+ }else{
+  //turn off...
+  chrome.browserAction.setIcon({ path: 'off.png'});
+  chrome.browserAction.setBadgeText({ text: '' });
+ }
 });
